b28b5b3a12381c83cda14d1cd3c8166277ca9b02
Jptrnmrs <jptrnmrs@gmail.com>
2025-02-07 15:43:07 +0900

    상세화면 api

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -72,7 +72,7 @@ class _MainScreenState extends ConsumerState<MainScreen> {
                     RankingPage(),
                     ListPage(),
                     isLoggedIn ? LoginView() : UserPage(),
-                    VideoDetailPage(item: dummyVideo),
+                    VideoDetailPage(item: "679c5eec7cf2875815230726"),
                   ],
                 ),
                 bottomNavigationBar: BottomNavigationBar(
