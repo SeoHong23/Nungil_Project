408f4d340c211f6e9e0904874d0cdb4d528cc2cc
SeoHong23 <ghd2408@naver.com>
2025-02-26 18:02:01 +0900

    카카오로그인

--- lib/screens/user/login/kakao_login.dart
+++ lib/screens/user/login/kakao_login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:nungil/providers/auth_provider.dart';
+import 'package:dio/dio.dart';
 
 class KakaoLoginService {
   final Ref ref;
@@ -8,28 +9,54 @@ class KakaoLoginService {
 
   Future<bool> kakaoLogin() async {
     try {
-      OAuthToken token;
-      if (await isKakaoTalkInstalled()) {
-        token = await UserApi.instance.loginWithKakaoTalk();
-      } else {
-        token = await UserApi.instance.loginWithKakaoAccount();
-      }
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+      print("카카오 로그인 성공: 액세스 토큰 : ${token.accessToken}");
+      // if (await isKakaoTalkInstalled()) {
+      //   token = await UserApi.instance.loginWithKakaoTalk();
+      // } else {
+      //   token = await UserApi.instance.loginWithKakaoAccount();
+      // }
 
       User user = await UserApi.instance.me();
+      String kakaoId = user.id.toString();
       String nickname = user.kakaoAccount?.profile?.nickname ?? "카카오 유저";
       String email = user.kakaoAccount?.email ?? "이메일 없음";
+      await sendTokenToBackend(token.accessToken, kakaoId, email, nickname);
 
-      ref.read(authProvider.notifier).login(
-            user.id?.toInt() ?? 0,
-            nickname,
-            email,
-            false
-          );
+      // ref.read(authProvider.notifier).login(
+      //       user.id?.toInt() ?? 0,
+      //       nickname,
+      //       email,
+      //       false,
+      //     );
 
-      return true; // 토큰 반환
+      return true;
     } catch (error) {
       print("카카오 로그인 실패: $error");
       return false;
+    }
+  }
+
+  Future<void> sendTokenToBackend(
+      String accessToken, String kakaoId, String email, String nickname) async {
+    try {
+      Response response = await Dio().post(
+        "http://13.239.238.92:8080/auth/kakao",
+        data: {
+          "access_token": accessToken,
+          "kakaoId": kakaoId,
+          "email": email,
+          "nickname": nickname,
+        },
+      );
+      final responseData = response.data;
+      final int userId = responseData['userId'] ?? 0;
+      final bool admin = responseData['admin'] ?? false;
+
+      ref.read(authProvider.notifier).login(userId, nickname, email, admin);
+      print("백엔드 로그인 : userId=$userId, nickname=$nickname, email=$email");
+    } catch (e) {
+      print("에러 :$e");
     }
   }
 }
